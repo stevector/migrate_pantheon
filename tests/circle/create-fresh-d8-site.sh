458041a9abf1e0c353a106d05fb92184dc0c730f
Steve Persch <steve.persch@pantheon.io>
2016-06-28 14:49:24 -0500

    Simplify terminus vars

--- tests/circle/create-fresh-d8-site.sh
+++ tests/circle/create-fresh-d8-site.sh
@@ -3,4 +3,4 @@
 # @todo, pull in upstream updates, wipe and install drupal.
 # Also, it might be cleaner to create an entirely new D8 site rather than making
 # multidevs off of the same one repeatedly.
-terminus site create-env --to-env=$PANTHEON_BRANCH --from-env=dev --site=$PANTHEON_SITE
+terminus site create-env --to-env=$TERMINUS_ENV --from-env=dev
