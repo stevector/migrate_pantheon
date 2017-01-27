42f2aa7836a5c81f214f66e0bf2c01188b703420
Steve Persch <steve.persch@pantheon.io>
2017-01-27 09:32:25 -0600

    Terminus 1 conversion

--- tests/circle/configure-migrations.sh
+++ tests/circle/configure-migrations.sh
@@ -2,27 +2,30 @@
 
 # Set the source database connection info in a secrets file where it can be
 # read by settings.migrate-on-pantheon.php
-export D7_MYSQL_URL=$(terminus site connection-info --site=$PANTHEON_D7_SITE --env=$PANTHEON_D7_BRANCH --field=mysql_url)
-terminus secrets set migrate_source_db__url $D7_MYSQL_URL
+export D7_MYSQL_URL=$(terminus connection:info $PANTHEON_D7_SITE.$PANTHEON_D7_BRANCH --field=mysql_url)
+terminus secrets:set $SITE_ENV migrate_source_db__url $D7_MYSQL_URL
+
 
 export PANTHEON_D7_URL="http://$PANTHEON_D7_BRANCH-$PANTHEON_D7_SITE.pantheonsite.io"
 
 # Run a cache clear to take time. Otherwise immediately enabling modules
 # after a code push might not work.
-terminus site clear-cache
-terminus drush "en -y migrate_plus migrate_tools migrate_upgrade"
-terminus site set-connection-mode --mode=sftp
-terminus drush "config-export -y"
+
+
+terminus env:clear-cache  $SITE_ENV
+terminus drush $SITE_ENV -- en -y migrate_plus migrate_tools migrate_upgrade
+terminus connection:set $SITE_ENV sftp
+terminus drush $SITE_ENV -- config-export -y
 
 # Make sure the source site is available.
-terminus site wake --site=$PANTHEON_D7_SITE --env=$PANTHEON_D7_BRANCH
+terminus  env:wake $PANTHEON_D7_SITE.$PANTHEON_D7_BRANCH
 
-terminus drush "migrate-upgrade --legacy-db-key=drupal_7 --configure-only  --legacy-root=$PANTHEON_D7_URL"
+terminus drush $SITE_ENV -- migrate-upgrade --legacy-db-key=drupal_7 --configure-only  --legacy-root=$PANTHEON_D7_URL
 # These cache rebuilds might not be necessary but I have seen odd errors
 # related to migration registration go away after cache-rebuild.
-terminus drush "cache-rebuild"
-terminus drush  "config-export -y"
-terminus drush "cache-rebuild"
+terminus drush $SITE_ENV -- cache-rebuild
+terminus drush $SITE_ENV -- config-export -y
+terminus drush $SITE_ENV -- cache-rebuild
 
 #terminus site code diffstat
 # @todo commit the code change. But there seems to be a multidev bug preventing
--- tests/circle/create-fresh-d8-site.sh
+++ tests/circle/create-fresh-d8-site.sh
@@ -3,4 +3,4 @@
 # @todo, pull in upstream updates, wipe and install drupal.
 # Also, it might be cleaner to create an entirely new D8 site rather than making
 # multidevs off of the same one repeatedly.
-terminus site create-env --to-env=$TERMINUS_ENV --from-env=dev
+terminus env:create  $TERMINUS_SITE.dev $TERMINUS_ENV
