69c544730745852d44f12d2e7902955ecb656d2b
Steve Persch <steve.persch@pantheon.io>
2017-01-27 10:16:54 -0600

    Moving wake higher

--- tests/circle/configure-migrations.sh
+++ tests/circle/configure-migrations.sh
@@ -2,23 +2,22 @@
 
 # Set the source database connection info in a secrets file where it can be
 # read by settings.migrate-on-pantheon.php
+
+terminus  env:wake $PANTHEON_D7_SITE.$PANTHEON_D7_BRANCH
 export D7_MYSQL_URL=$(terminus connection:info $PANTHEON_D7_SITE.$PANTHEON_D7_BRANCH --field=mysql_url)
 terminus secrets:set $SITE_ENV migrate_source_db__url $D7_MYSQL_URL
-
 
 export PANTHEON_D7_URL="http://$PANTHEON_D7_BRANCH-$PANTHEON_D7_SITE.pantheonsite.io"
 
 # Run a cache clear to take time. Otherwise immediately enabling modules
 # after a code push might not work.
-
-
 terminus env:clear-cache  $SITE_ENV
 terminus drush $SITE_ENV -- en -y migrate_plus migrate_tools migrate_upgrade
 terminus connection:set $SITE_ENV sftp
 terminus drush $SITE_ENV -- config-export -y
 
 # Make sure the source site is available.
-terminus  env:wake $PANTHEON_D7_SITE.$PANTHEON_D7_BRANCH
+
 
 terminus drush $SITE_ENV -- migrate-upgrade --legacy-db-key=drupal_7 --configure-only  --legacy-root=$PANTHEON_D7_URL
 # These cache rebuilds might not be necessary but I have seen odd errors
