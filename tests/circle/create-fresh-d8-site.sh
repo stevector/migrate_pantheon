dda937a7174d5207f9683290b4d3cc4a3a745bff
Steve Persch <stevector@users.noreply.github.com>
2017-06-30 14:34:08 -0500

    Update create-fresh-d8-site.sh

--- tests/circle/create-fresh-d8-site.sh
+++ tests/circle/create-fresh-d8-site.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 terminus upstream:updates:apply $TERMINUS_SITE.dev --yes --accept-upstream --updatedb
+terminus upstream:updates:apply $PANTHEON_D7_SITE.dev --yes --accept-upstream --updatedb
 
 # @todo, pull in upstream updates, wipe and install drupal.
 # Also, it might be cleaner to create an entirely new D8 site rather than making
 # multidevs off of the same one repeatedly.
-terminus env:create  $TERMINUS_SITE.dev $TERMINUS_ENV
+terminus env:create $TERMINUS_SITE.dev $TERMINUS_ENV
