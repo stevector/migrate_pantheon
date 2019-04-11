a88e57538ca408f5173b5394e8aeb10adfcdce97
Steve Persch <stevector@users.noreply.github.com>
2019-04-10 23:11:15 -0700

    Update setup-global-vars.sh

--- setup-global-vars.sh
+++ setup-global-vars.sh
@@ -3,6 +3,8 @@
 set -ex
 
 mkdir -p $HOME/.ssh && echo "StrictHostKeyChecking no" >> "$HOME/.ssh/config"
+# Set up BASH_ENV if it was not set for us.
+BASH_ENV=${BASH_ENV:-$HOME/.bashrc}
 
 (
   echo 'export TERMINUS_ENV=${CIRCLE_BUILD_NUM}'
