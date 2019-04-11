70fc3b98f496d3ee9d7d91bbbafdb13c6ba6c39e
Steve Persch <steve.persch@pantheon.io>
2019-04-10 23:19:16 -0700

    more circle cleanup

--- .circleci/tests/configure-migrations.sh
+++ .circleci/tests/configure-migrations.sh
@@ -3,7 +3,6 @@
 # Set the source database connection info in a secrets file where it can be
 # read by settings.migrate-on-pantheon.php
 
-
 cd drupal8
 terminus drush $SITE_ENV -- status
 terminus  env:wake $PANTHEON_D7_SITE.$PANTHEON_D7_BRANCH
@@ -19,7 +18,7 @@ terminus drush $SITE_ENV -- en -y migrate_plus migrate_tools migrate_upgrade
 terminus connection:set $SITE_ENV sftp
 terminus drush $SITE_ENV -- config-export -y
 
-terminus drush $SITE_ENV -- migrate-upgrade --legacy-db-key=drupal_7 --configure-only  --legacy-root=$PANTHEON_D7_URL
+terminus drush $SITE_ENV -- migrate-upgrade --legacy-db-key=drupal_7 --configure-only --legacy-root=$PANTHEON_D7_URL
 # These cache rebuilds might not be necessary but I have seen odd errors
 # related to migration registration go away after cache-rebuild.
 terminus drush $SITE_ENV -- cache-rebuild
--- .circleci/tests/create-fresh-d8-site.sh
+++ .circleci/tests/create-fresh-d8-site.sh
@@ -2,8 +2,4 @@
 
 terminus upstream:updates:apply $TERMINUS_SITE.dev --yes --accept-upstream --updatedb
 terminus upstream:updates:apply $PANTHEON_D7_SITE.dev --yes --accept-upstream --updatedb
-
-# @todo, pull in upstream updates, wipe and install drupal.
-# Also, it might be cleaner to create an entirely new D8 site rather than making
-# multidevs off of the same one repeatedly.
 terminus env:create $TERMINUS_SITE.dev $TERMINUS_ENV
