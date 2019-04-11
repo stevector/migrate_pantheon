2629e999390f3ebd7d0ee2447c54df3168788861
Steve Persch <stevector@users.noreply.github.com>
2019-04-11 00:25:44 -0700

    separating steps (#43)
    * separating steps
    
    * adding vars script
    
    * adding vars script
    
    * build tools 4.x
    
    * Update setup-global-vars.sh
    
    * Update setup-global-vars.sh
    
    * Update config.yml
    
    * Update setup-global-vars.sh
    
    * Update config.yml

---
+++ setup-global-vars.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -ex
+
+mkdir -p $HOME/.ssh && echo "StrictHostKeyChecking no" >> "$HOME/.ssh/config"
+# Set up BASH_ENV if it was not set for us.
+BASH_ENV=${BASH_ENV:-$HOME/.bashrc}
+touch $BASH_ENV
+
+(
+  echo 'export TERMINUS_ENV=${CIRCLE_BUILD_NUM}'
+  echo 'export SITE_ENV=${TERMINUS_SITE}.${TERMINUS_ENV}'
+) >> $BASH_ENV
+
+source $BASH_ENV
