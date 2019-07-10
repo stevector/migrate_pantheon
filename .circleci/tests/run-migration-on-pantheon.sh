4056e429ee4da1e7d1ea06338d1453db29f5f49e
Steve Persch <steve.persch@pantheon.io>
2019-07-10 16:06:28 -0500

    circle cleanup again

--- .circleci/tests/run-migration-on-pantheon.sh
+++ .circleci/tests/run-migration-on-pantheon.sh
@@ -4,8 +4,8 @@
 terminus  env:wake $PANTHEON_D7_SITE.$PANTHEON_D7_BRANCH
 
 # Report the status of migrations before and after running migrations.
-terminus drush $SITE_ENV -- migrate-status
+terminus drush $TERMINUS_SITE.$TERMINUS_ENV -- migrate-status
 # Run all the migrations.
-terminus drush $SITE_ENV -- migrate-import --all
+terminus drush $TERMINUS_SITE.$TERMINUS_ENV -- migrate-import --all
 # Report the status of migrations before and after running migrations.
-terminus drush $SITE_ENV -- migrate-status
+terminus drush $TERMINUS_SITE.$TERMINUS_ENV -- migrate-status
